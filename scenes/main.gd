cdbf51934891e17a9a3a518d6f2ee6e9368d6906
Daniel Thornton <dshaggy28@yahoo.com>
2023-12-06 19:11:06 -0800

    Worked on next player panel.

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,5 +1,6 @@
 extends Node
 
+@onready var player_panel = $PlayerPanel
 @onready var board = $Board
 @export var circle_scene: PackedScene
 @export var cross_scene: PackedScene
@@ -7,8 +8,14 @@ extends Node
 var player: int
 var grid_data: Array
 var grid_pos: Vector2i
+var player_panel_pos: Vector2i
 var board_size: int
 var cell_size: int
+var temp_marker
+var row_sum: int
+var col_sum: int
+var diagonal1_sum: int
+var diagonal2_sum: int
 
 
 # Called when the node enters the scene tree for the first time.
@@ -16,6 +23,8 @@ func _ready():
 	board_size = board.texture.get_width()
 	# Divide board by 3 to get the individual size of each cell.
 	cell_size = board_size / 3
+	# Position on the panel on the right side of the board.
+	player_panel_pos = player_panel.get_position()
 	new_game()
 	
 
@@ -35,8 +44,9 @@ func _input(event):
 					player *= -1 
 					# Place the players marker
 					create_marker(player, grid_pos * cell_size + Vector2i(cell_size / 2, cell_size / 2))
-					
-				
+					# update the marker.
+					temp_marker.queue_free() 
+					create_marker(player, player_panel_pos + Vector2i(cell_size / 2, cell_size / 2), true)
 func new_game():
 	player = 1
 	grid_data = [
@@ -44,15 +54,19 @@ func new_game():
 		[0,0,0],
 		[0,0,0]
 		]
+	 # Maker for the starting player's turn
+	create_marker(player, player_panel_pos + Vector2i(cell_size / 2, cell_size / 2), true)
 
-func create_marker(player, position):
+func create_marker(player, position, temp=false):
 	if player == 1:
 		var circle = circle_scene.instantiate()
 		circle.position = position
 		add_child(circle)
+		if temp: temp_marker = circle 
 	else:
 		var cross = cross_scene.instantiate()
 		cross.position = position
 		add_child(cross)
+		if temp: temp_marker = cross
 
 
