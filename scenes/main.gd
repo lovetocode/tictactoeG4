a223a89723da0aeb7861201410fd0d80b3141bf7
Daniel Thornton <dshaggy28@yahoo.com>
2023-12-08 17:31:25 -0800

    Added game over menu

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,5 +1,7 @@
 extends Node
 
+
+@onready var game_over = $GameOver
 @onready var player_panel = $PlayerPanel
 @onready var board = $Board
 @export var circle_scene: PackedScene
@@ -12,6 +14,7 @@ var player_panel_pos: Vector2i
 var board_size: int
 var cell_size: int
 var temp_marker
+var winner: int
 var row_sum: int
 var col_sum: int
 var diagonal1_sum: int
@@ -44,19 +47,32 @@ func _input(event):
 					player *= -1 
 					# Place the players marker
 					create_marker(player, grid_pos * cell_size + Vector2i(cell_size / 2, cell_size / 2))
+					if check_win() != 0:
+						get_tree().paused = true
+						game_over.show()
 					# update the marker.
 					temp_marker.queue_free() 
 					create_marker(player, player_panel_pos + Vector2i(cell_size / 2, cell_size / 2), true)
 func new_game():
 	player = 1
+	winner = 0
 	grid_data = [
 		[0,0,0],
 		[0,0,0],
 		[0,0,0]
 		]
+	# clear game
+	row_sum = 0
+	col_sum = 0
+	diagonal1_sum = 0
+	diagonal2_sum = 0
+	get_tree().call_group("circle", "queue_free")
+	get_tree().call_group("crosses", "queue_free")
 	 # Maker for the starting player's turn
 	create_marker(player, player_panel_pos + Vector2i(cell_size / 2, cell_size / 2), true)
-
+	game_over.hide()
+	get_tree().paused = false
+	
 func create_marker(player, position, temp=false):
 	if player == 1:
 		var circle = circle_scene.instantiate()
@@ -69,4 +85,21 @@ func create_marker(player, position, temp=false):
 		add_child(cross)
 		if temp: temp_marker = cross
 
+func check_win():
+	# add up the markers for each row, column, and diagonal
+	for i in len(grid_data):
+		row_sum = grid_data[i][0] + grid_data[i][1] + grid_data[i][2]
+		col_sum = grid_data[0][i] + grid_data[1][i] + grid_data[2][i]
+		diagonal1_sum = grid_data[0][0] + grid_data[1][1] + grid_data[2][2]
+		diagonal2_sum = grid_data[0][2] + grid_data[1][1] + grid_data[2][0]
+		
+		if row_sum == 3 or col_sum == 3 or diagonal1_sum == 3 or diagonal2_sum == 3:
+			winner = 1
+		elif row_sum == -3 or col_sum == -3 or diagonal1_sum == -3 or diagonal2_sum == -3:
+			winner = -1
+	return winner
+		
 
+
+func _on_game_over_restart():
+	new_game()
